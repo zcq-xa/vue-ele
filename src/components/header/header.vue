64ae0bc6e087f4e210e41ccdf122506fbf4ed8ac
zcq-xa <zhangchuqiang@gmail.com>
2017-05-18 11:16:19 +0800

    VUE-1.0

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,15 +17,24 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background"><img :src="seller.avatar" width="100%" height="100%"></div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close"><i class="icon-close"></i></div>
     </div>
   </div>
 </template>
@@ -34,6 +43,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
       }
     },
     created() {
@@ -150,5 +169,40 @@
         font-size: 10px
         right: 12px
         top: 8px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background-color: rgba(7, 17, 27, .8)
+      .detail-wrapper
+        width: 100%
+        min-height: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0
+        clear: both
+        font-size: 32px
 </style>
