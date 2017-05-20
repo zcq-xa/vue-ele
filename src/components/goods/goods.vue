beb8a6d91619dc67ff0ccf867789ae5574659cc1
zcq-xa <zhangchuqiang@gmail.com>
2017-05-20 15:32:03 +0800

    VUE-1.0

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -12,10 +12,23 @@
     <div class="foods-wrapper">
       <ul>
         <li v-for="item in goods" class="food-list">
-          <h1>{{item.name}}</h1>
+          <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food-item"></li>
-            <div></div>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon"><img :src="food.icon"></div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
           </ul>
         </li>
       </ul>
@@ -68,7 +81,7 @@
         display: table
         height: 54px
         width: 56px
-        margin:0 auto
+        margin: 0 auto
         line-height: 14px
         .icon
           display: inline-block
@@ -97,4 +110,59 @@
     .foods-wrapper
       flex: 1
       background: #fff;
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(#f3f5f7)
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          width: 57px
+          height: 57px
+          margin-right: 10px
+          img
+            width: 100%
+            height: 100%
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            margin-bottom: 8px
+          .extra
+            &.count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
+
+
 </style>
