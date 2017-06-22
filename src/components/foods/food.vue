947353d65c374818f96df4a0fbc7388f95452de8
zcq-xa <zhangchuqiang@gmail.com>
2017-06-22 11:35:05 +0800

    vue 1.0

---
+++ src/components/foods/food.vue
@@ -0,0 +1,52 @@
+<template>
+  <div v-show="showFlag" class="food" transition="move">
+    <div class="food-content">
+      <div class="image-header"><img :src="food.image"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      food: Object
+    },
+    data() {
+      return {
+        showFlag: false
+      }
+    },
+    methods: {
+      show() {
+        this.showFlag = true
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" type="text/stylus">
+  .food
+    position: fixed
+    left: 0
+    top: 0
+    bottom: 48px
+    z-index: 30
+    width: 100%
+    background: #fff
+    &.move-transition
+      transition: all 0.2s linear
+      transform: translate3d(0, 0, 0)
+    &.move-enter, &.move-leave
+      transform: translate3d(100%, 0, 0)
+    .image-header
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+</style>
